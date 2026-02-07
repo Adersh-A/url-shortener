1c3f32a3c2d87d0345765b1eb1c5ac97e75ead05
Adersh A <43125084+Adersh-A@users.noreply.github.com>
2026-02-07 16:01:02 +0530

    ui redesign

--- src/main/java/com/myprojects/urlshortener/config/WebSecurityConfig.java
+++ src/main/java/com/myprojects/urlshortener/config/WebSecurityConfig.java
@@ -21,26 +21,25 @@ public class WebSecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-            .csrf(CsrfConfigurer::disable)
-            .authorizeHttpRequests(authorize -> authorize
-                .requestMatchers(
-                    "/error", "/webjars/**", "/css/**", "/js/**", "/images/**",
-                    "/", "/short-urls", "/s/**", "/register", "/login"
-                ).permitAll()
-                .requestMatchers("/my-urls").authenticated()
-                .requestMatchers("/admin/**").hasRole("ADMIN")
-                .anyRequest().authenticated()
-            )
-            .formLogin(form -> form
-                .loginPage("/login")
-                .defaultSuccessUrl("/")
-                .permitAll()
-            )
-            .logout(logout -> logout
-                .logoutUrl("/logout")
-                .logoutSuccessUrl("/login?logout")
-                .permitAll()
-            );
+                .csrf(CsrfConfigurer::disable)
+                .headers(headers -> headers
+                        .cacheControl(cache -> {
+                        })
+                )
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers(
+                                "/error", "/webjars/**", "/css/**", "/js/**", "/images/**",
+                                "/", "/short-urls", "/s/**", "/register", "/login"
+                        ).permitAll()
+                        .requestMatchers("/my-urls").authenticated()
+                        .requestMatchers("/admin/**").hasRole("ADMIN")
+                        .anyRequest().authenticated()
+                )
+                .formLogin(form -> form
+                        .loginPage("/login")
+                        .defaultSuccessUrl("/")
+                        .permitAll()
+                );
 
         return http.build();
     }
